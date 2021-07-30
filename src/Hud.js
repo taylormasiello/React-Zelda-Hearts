865d9ceda7f1528986339b061906f810b37f087b
TaylorMasiello <tmasiello@echobind.com>
2021-07-30 08:08:18 -0700

    Starting point to create heart HUD

---
+++ src/Hud.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Hearts from './Hearts'
+import MinMaxButton from './MinMaxButton'
+//import {Heart} from "./icons";
+
+export default class Hud extends React.Component {
+
+  state = {
+    hearts: 3,
+    maxHearts: 5,
+  };
+
+  render() {
+    return (
+      <div className={"Hud"}>
+      I AM THE HUD          
+        <aside className="Sidebar">
+          <MinMaxButton
+            value={this.state.hearts}
+            maxValue={this.state.maxHearts}
+            increment={0.5}
+            onNewValue={(newValue) => {
+              //TODO
+            }}
+          />
+        </aside>
+      </div>
+    );
+  }
+}
